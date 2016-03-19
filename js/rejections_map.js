054789809df3341d96fc28ceaddf6eda6dbe1272
Niko Kommenda <nkommenda@hotmail.com>
2016-03-19 11:48:18 +0000

    basic map of ratios data

--- js/rejections_map.js
+++ js/rejections_map.js
@@ -1,5 +1,47 @@
-var map = new L.Map('map', {zoom : 4})
-	.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+var map = new L.Map('map', {zoom : 4}).setView([43.555073, 2.580898], 2);
 
-var svg = d3.select(map.getPanes().overlayPane).append('svg');
-var g = svg.append('g').attr('class', 'leaflet-zoom-hide');
+var ratios = {};
+
+var ratiosScale = chroma.scale(['blue', '#eee', 'red']).domain([0,0.35]);
+
+var styleByRatios = function(feature){
+
+	var style = {
+		fillOpacity : 1,
+		opacity : 1,
+		color : '#000',
+		weight: 0.5
+	}
+
+	col = ratiosScale(parseFloat(ratios[feature.id]));
+
+	style.fillColor = col;
+
+	return style;
+}
+
+d3.json('geography/countries.geo.json', function(error, json) {
+
+	if(error) { return console.warn(error); }
+
+	console.log(json);
+
+	d3.csv('datasets/ratios_alpha3.csv', function(d) {
+
+		for(var row of d) {
+			//console.log(row);
+			ratios[row['country_alpha3']] = row['rejection_rate'];
+
+		}
+
+		console.log(ratios);
+
+		L.geoJson(json,
+			{ style : styleByRatios })
+		.addTo(map);
+
+		//console.log(ratios);
+
+	});
+
+});
