b0c08148c0f94ba62e3bb5d625264b37af84dc3a
Niko Kommenda <nkommenda@hotmail.com>
2016-05-11 21:31:19 +0100

    visually improved savings calculator

--- js/savingscalculator.js
+++ js/savingscalculator.js
@@ -6,7 +6,7 @@ savingsCalculator = function() {
 
   var result = Math.round((Math.log(  ((q - 1) * 62500) / x + 1)/Math.log(q))/12)
 
-  div.innerHTML = 'It would take you ' +result+ ' years to save the &pound;62,500 needed to settle your spouse in the UK'
+  div.innerHTML = 'It would take you ' +result+ ' years to save the &pound;62,500 needed to settle your spouse in the UK.'
 
   console.log(result)
   return false;
